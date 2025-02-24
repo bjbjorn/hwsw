0395f6238b8f614899bc3c1b58f4e09bcbf44274
Bjorn Spauwen <bjorn.spauwen@tuni.fi>
2025-02-24 16:29:10 +0100

    push to roger

--- ch1/firmware/assignment/src/print.c
+++ ch1/firmware/assignment/src/print.c
@@ -16,20 +16,58 @@ void print_str(const char *p) {
 		*((volatile unsigned int*)OUTPORT) = *(p++);
 }
 
+unsigned int base_lookup[] = {0,1,10,100,1000,10000,100000,1000000,10000000,100000000,1000000000};
+
 void print_dec(unsigned int val) {
-    char buffer[11]; // Enough to hold the maximum 32-bit unsigned integer value and a null terminator
-    int i = 10;
-    buffer[i] = '\0'; // Null-terminate the string
-
-	// Convert the integer to a string using modulo and division
-	do {
-		buffer[--i] = '0' + (val % 10);
-		val /= 10;
-	} while (val > 0);
-
-    // Pass the resulting string to print_str
-    print_str(&buffer[i]);
+    char leading_0_flag = 0;
+    for (int i = 10; i > 0; i--)
+    {
+        //print_str("i: ");
+        //print_hex(i,8);
+        //print_str("\n");
+        int index = getDigit(base_lookup[i],val);
+        if (index != 0 || leading_0_flag != 0)
+        {
+            leading_0_flag = 1;
+            char x="0123456789"[index];
+            *((volatile unsigned int*)OUTPORT) = x;
+        }
+        for (int j = 0; j < index; j++){
+            val -= base_lookup[i];
+        }
+    }
+    print_str("\n");
+    return;
 }
+int getDigit(int base, int number) {
+    //print_str("number: ");
+    //print_hex(number,8);
+    //print_str("\n");
+    //print_str("base: ");
+    //print_hex(base,8);
+    //print_str("\n");
+    int digit = 0;
+    for (int i = base;i <= number;i += base) ++digit;
+    //print_str("digit: ");
+    //print_hex(digit,1);
+    //print_str("\n");
+    return digit;
+}
+
+// void print_dec(unsigned int val) {
+//     char buffer[11]; // Enough to hold the maximum 32-bit unsigned integer value and a null terminator
+//     int i = 10;
+//     buffer[i] = '\0'; // Null-terminate the string
+
+// 	// Convert the integer to a string using modulo and division
+// 	do {
+// 		buffer[--i] = '0' + (val % 10);
+// 		val /= 10;
+// 	} while (val > 0);
+
+//     // Pass the resulting string to print_str
+//     print_str(&buffer[i]);
+// }
 
 void print_hex(unsigned int val, int digits) {
 	unsigned int index, max;
