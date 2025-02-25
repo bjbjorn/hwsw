3315fccf3fc54527df28a3a6d57fdb0800b2ba24
Bjorn Spauwen <bjorn.spauwen@tuni.fi>
2025-02-25 10:31:15 +0100

    push to roger

--- ch1/firmware/assignment/src/firmware.c
+++ ch1/firmware/assignment/src/firmware.c
@@ -11,6 +11,29 @@ int main(void) {
 	print_dec(temp);
 }
 
+int divide(int dividend, int divisor) {
+    if (divisor == 0) {
+        return -1;
+    }
+
+    int quotient = 0;
+    int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
+
+    unsigned int a = (dividend < 0) ? -dividend : dividend;
+    unsigned int b = (divisor < 0) ? -divisor : divisor;
+
+    while (a >= b) {
+        unsigned int temp = b, multiple = 1;
+        while (a >= (temp << 1) && (temp << 1) > temp) {
+            temp <<= 1;
+            multiple <<= 1;
+        }
+        a -= temp;
+        quotient += multiple;
+    }
+    return sign * quotient;
+}
+
 unsigned int convert(unsigned int x) {
 	unsigned int result, temp, temp2;
 	temp = x - 32;
--- ch1/firmware/assignment/src/print.c
+++ ch1/firmware/assignment/src/print.c
@@ -18,29 +18,6 @@ void print_str(const char *p) {
 		*((volatile unsigned int*)OUTPORT) = *(p++);
 }
 
-int divide(int dividend, int divisor) {
-    if (divisor == 0) {
-        return -1;
-    }
-
-    int quotient = 0;
-    int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
-
-    unsigned int a = (dividend < 0) ? -dividend : dividend;
-    unsigned int b = (divisor < 0) ? -divisor : divisor;
-
-    while (a >= b) {
-        unsigned int temp = b, multiple = 1;
-        while (a >= (temp << 1) && (temp << 1) > temp) {
-            temp <<= 1;
-            multiple <<= 1;
-        }
-        a -= temp;
-        quotient += multiple;
-    }
-    return sign * quotient;
-}
-
 int getDigit(int base, int number) {
     int digit = 0;
     for (int i = base;i <= number;i += base) ++digit;
