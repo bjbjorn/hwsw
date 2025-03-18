256e06acb3adc191061b80398481a52fa21599aa
Bjorn Spauwen <bjorn.spauwen@tuni.fi>
2025-03-18 14:40:01 +0100

    push to roger

--- assignment4/assignment4/firmware/course_example_1/src/firmware.c
+++ assignment4/assignment4/firmware/course_example_1/src/firmware.c
@@ -1,8 +1,11 @@
-//#include "tcnt.h"
-
 #define LED_BASEAxDDRESS 0x80000000
+
 #define LED_REG0_ADDRESS (LED_BASEAxDDRESS + 0*4)
+
 #define LED              (*(volatile unsigned int *) LED_REG0_ADDRESS)
+
+
+
 
 void irq_handler(unsigned int cause) {
 
@@ -16,12 +19,11 @@ void irq_handler(unsigned int cause) {
 void main(void) {
     
     unsigned int i=1, j;
-    //TCNR_start();
 
-    for(j=0;j<10;j++) {
+
+    while(1) {
         for(i=0;i<8;i++) {
             LED = i;
         }
     }
-    //TCNT_stop();
 }
