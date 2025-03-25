58dc6b9d9fcee18056860a9730b6f50d8a44f5c1
Bjorn Spauwen <bjorn.spauwen@tuni.fi>
2025-03-25 21:46:26 +0100

    push to roger

--- assignment4/assignment4/firmware/course_example_1/src/firmware.c
+++ assignment4/assignment4/firmware/course_example_1/src/firmware.c
@@ -11,22 +11,16 @@ unsigned int leds_incr = 0;
 
 
 void irq_handler(unsigned int cause) {
-
-/*     if (cause & 4) {
-        LED = 0xFFFFFFFF;
-    } */
     leds_incr = 0x1;
     TCNT_CR = 0x17;
     TCNT_CR = 0x7;
-
 }
 
 
 void main(void) {
     unsigned int i = 0;
-//    unsigned int i=1, j;
-      TCNT_CMP = 0xc65d40;
-//    TCNT_CMP = 0x100;
+//    TCNT_CMP = 0xc65d40;  // for pynq board
+    TCNT_CMP = 0x100;   // for simulation
     TCNT_start();
     while(1) {
         if (leds_incr) {
