fbfb43ee5233f714dedec42386fad49a250a9883
Bjorn Spauwen <bjorn.spauwen@tuni.fi>
2025-04-23 13:59:53 +0200

    push to roger

--- project/finalproject/firmware/finalproject/src/firmware.c
+++ project/finalproject/firmware/finalproject/src/firmware.c
@@ -47,11 +47,38 @@ int main(void) {
     unsigned char C_WIDTH = SENSOR_get_width();
     unsigned char C_HEIGHT = SENSOR_get_height();
 
-
     unsigned char r[C_HEIGHT][C_WIDTH];
     unsigned char g[C_HEIGHT][C_WIDTH];
     unsigned char b[C_HEIGHT][C_WIDTH];
     unsigned char a[C_HEIGHT][C_WIDTH];
+    
+    //initialise(r, g, b, a);
+    for (unsigned char h = 0; h < C_HEIGHT; h++) {
+        for (unsigned char w = 0; w < C_WIDTH; w++) {
+            unsigned int pixel = SENSOR_fetch();
+            r[h][w] = (pixel >> 24) & 0xFF;
+            g[h][w] = (pixel >> 16) & 0xFF;
+            b[h][w] = (pixel >> 8)  & 0xFF;
+            a[h][w] = (pixel >> 0)  & 0xFF;
+        }
+    }
+    
+    unsigned char r_prev = 0;
+    unsigned char g_prev = 0;
+    unsigned char b_prev = 0;
+    unsigned char a_prev = 255;
+    
+    signed char dr;
+    signed char dg;
+    signed char db;
+    
+    signed char rle = -1;
+    unsigned int running_array[64];
+
+
+    for(unsigned char i = 0; i < 64; i++) {
+        running_array[i] = 0;
+    }
 
     /* Header */
     OUTPUT = (unsigned char)'q';
@@ -72,4 +99,89 @@ int main(void) {
     OUTPUT = 0x03;
     OUTPUT = 0x00;
 
+    for(unsigned char h = 0; h < C_HEIGHT; h++) {
+        for(unsigned char w = 0; w < C_WIDTH; w++) {
+            unsigned char cr = r[h][w];
+            unsigned char cg = g[h][w];
+            unsigned char cb = b[h][w];
+            unsigned char ca = a[h][w];
+
+            if (cr == r_prev && cg == g_prev && cb == b_prev && ca == a_prev) {
+                if (rle == -1) rle = 1;
+                else rle++;
+
+                if (rle == 62) {
+                    OUTPUT = (0b11000000 | (rle - 1));
+                    rle = -1;
+                }
+                continue;
+            }
+
+            if (rle > 0) {
+                OUTPUT = (0b11000000 | (rle - 1));
+                rle = -1;
+            }
+
+            unsigned char index_pos = (sw_mult(cr,3) + sw_mult(cg,5) + sw_mult(cb,7)+ sw_mult(ca,11)) & 63;
+            unsigned int packed = ((unsigned int)cr << 24) | ((unsigned int)cg << 16) | ((unsigned int)cb << 8)  | (unsigned int)ca;
+
+            if (running_array[index_pos] == packed) {
+                OUTPUT = index_pos;
+            } else {
+                running_array[index_pos] = packed;
+
+                dr = cr - r_prev;
+                dg = cg - g_prev;
+                db = cb - b_prev;
+
+                if (ca == a_prev &&
+                    dr >= -2 && dr <= 1 &&
+                    dg >= -2 && dg <= 1 &&
+                    db >= -2 && db <= 1) {
+                    unsigned char diff_byte = 0b01000000 |
+                        ((dr + 2) << 4) |
+                        ((dg + 2) << 2) |
+                        (db + 2);
+                    OUTPUT = diff_byte;
+                    r_prev = cr;
+                    g_prev = cg;
+                    b_prev = cb;
+                    continue;
+                }
+
+                if (ca == a_prev) {
+                    OUTPUT = 0xFE;
+                    OUTPUT = cr;
+                    OUTPUT = cg;
+                    OUTPUT = cb;
+                } else {
+                    OUTPUT = 0xFF;
+                    OUTPUT = cr;
+                    OUTPUT = cg;
+                    OUTPUT = cb;
+                    OUTPUT = ca;
+                }
+            }
+
+            r_prev = cr;
+            g_prev = cg;
+            b_prev = cb;
+            a_prev = ca;
+        }
+    }
+    if (rle > 0) {
+        OUTPUT = (0b11000000 | (rle - 1));
+        rle = -1;
+    }
+
+    OUTPUT = 0x00;
+    OUTPUT = 0x00;
+    OUTPUT = 0x00;
+    OUTPUT = 0x00;
+    OUTPUT = 0x00;
+    OUTPUT = 0x00;
+    OUTPUT = 0x00;
+    OUTPUT = 0x01;
+
+    return 0;
 }
